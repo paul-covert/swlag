40799552952f958e44bcc6fad9caec150e2a72ed
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-07 13:31:09 -0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import pyuwtc as uwtc
 
 test_fname = "./data/2023-026-20230826.tob"
 df = uwtc.read_tob(test_fname)
-print(df)
+df["temp_prime"] = df["temp_inlet"] + 0.1
+fig, ax = uwtc.plot_tsg_flow(df)
+fig2, ax2 = uwtc.plot_temp(df)
+plt.show()
