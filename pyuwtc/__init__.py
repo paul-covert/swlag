60c43491cb69be349895f248805aa2ce4813cbe6
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-07 10:12:37 -0500

    Added docstring

--- pyuwtc/__init__.py
+++ pyuwtc/__init__.py
@@ -1,1 +1,20 @@
+# pyuwtc: Lag and averaging time constant calculation in Python.
+# Copyright (C) 2025  Paul A. Covert (GNU GPLv3)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Calculate lag and averaging time constants for surface underway seawater
+systems on Canadian Coast Guard vessels based at the Institute of Ocean 
+Sciences in Sidney, BC."""
+
 from .io import read_tob
