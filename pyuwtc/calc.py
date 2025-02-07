e1790fbdf8f89dc6a9631c427f5a26431fbb4316
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-07 10:13:12 -0500

    Update calc.py

--- pyuwtc/calc.py
+++ pyuwtc/calc.py
@@ -2,5 +2,6 @@ def errfunc(T_lab, T_inlet_prime):
     return ((T_lab - T_inlet_prime)**2).sum()
     
 def transform_inlet_temp(T, dt, dT, tau):
-    
-    
+    T_prime = T.shift(int(dt / 5))
+    T_prime = T_prime.rolling(int(tau / 5), center=False).mean()
+    T_prime = T_prime + dT
