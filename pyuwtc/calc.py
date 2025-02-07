9916358fe3c7303e57ef59bee0157d24aa307f0b
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-07 13:30:56 -0500

    Update calc.py

--- pyuwtc/calc.py
+++ pyuwtc/calc.py
@@ -1,8 +1,22 @@
-def errfunc(T_lab, T_inlet_prime):
-    return ((T_lab - T_inlet_prime) ** 2).sum()
+def _check_timeseries_spacing(df):
+    """Ensure monotonically increasing time"""
+    import numpy as np
+    
+    diff = np.diff(df.index.to_numpy())
+    return np.all(diff==diff[0])
 
 
 def transform_inlet_temp(T, dt, dT, tau):
     T_prime = T.shift(int(dt / 5))
     T_prime = T_prime.rolling(int(tau / 5), center=False).mean()
     T_prime = T_prime + dT
+    return T_prime
+
+
+def errfunc(Tinlet, Tlab):
+    Tinlet_prime = transform_inlet_temp(T, dt, dT, tau)
+    return ((Tlab - Tinlet) ** 2).sum()
+
+
+def calc_time_consts(Tinlet, Tlab, dt0, dT0, tau0):
+    
