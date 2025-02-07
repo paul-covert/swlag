494f1038efd31991fa2e0616a89d4905da843f93
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-07 10:12:59 -0500

    Create meta.py

---
+++ pyuwtc/meta.py
@@ -0,0 +1,15 @@
+# pyuwtc: Lag and averaging time constant calculation in Python.
+# Copyright (C) 2025  Paul A. Covert (GNU GPLv3)
+"""Define metadata about pyuwtc."""
+
+version = "25.02.1a1"
+version_xy = "25.02"
+authorlist = [
+    "Covert, Paul A.",
+]
+authors = " and ".join(authorlist)
+
+
+def hello():
+    print("pyuwtc: Lag and averaging time constant calculation in Python.")
+    print("        Version {} :: doi:".format(version_xy)
