565735b9edf271ba16006017913afd634c4782f8
Paul Covert <82682319+paul-covert@users.noreply.github.com>
2025-02-10 10:57:51 -0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,11 @@
-#! /usr/bin/env python3
-
+import pandas as pd
 import swlag as lag
 
 fname = "./data/2023-026-20230827.tob"
 df = lag.read_tob(fname)
 
+def test_io():
+    """Does read_tob() return a valid pandas DataFrame?"""
+    assert isinstance(df, pd.DataFrame)
+
+# test_io()
