5db3af5f7fd757ac3126d474aded501fa91464ab
Paul Covert <82682319+paul-covert@users.noreply.github.com>
2025-02-10 10:48:44 -0500

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,7 @@
 #! /usr/bin/env python3
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import pyuwtc as uwtc
+import swlag as lag
 
-test_fnames = [
-    "./data/2023-026-20230825.tob",
-    "./data/2023-026-20230826.tob",
-    "./data/2023-026-20230827.tob",
-    "./data/2023-026-20230828.tob",
-    "./data/2023-026-20230829.tob"
-]
-df = pd.concat([uwtc.read_tob(test_fname) for test_fname in test_fnames])
-df["temp_prime"] = df["temp_inlet"] + 0.1
-fig, ax = uwtc.plot_tsg_flow(df)
-fig2, ax2 = uwtc.plot_temp(df)
-plt.show()
+fname = "./data/2023-026-20230827.tob"
+df = lag.read_tob(fname)
+
