41517079537c03cd6fbdb9d7a17b4a8645ef1aa7
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-04-15 08:55:44 -0700

    Update calc.py

--- swlag/calc.py
+++ swlag/calc.py
@@ -4,20 +4,90 @@
 
 
 def _check_timeseries_spacing(df):
-    """Ensure monotonically increasing time"""
+    """
+    Ensure uniformly, monotonically increasing time
+    
+    Parameters
+    ----------
+    df : Pandas dataframe indexed by datetime64
+        Contains the time (index), inlet temperature, and
+        lab temperature series.
+    
+    Returns
+    -------
+    monotonic : boolean
+        True of index of df is monotonically increasing.
+    
+    """
     import numpy as np
-
+    
     diff = np.diff(df.index.to_numpy())
-    return np.all(diff == diff[0])
+    monotonic = np.all(diff==diff[0])
+    
+    return monotonic
 
 
-def transform_inlet_temp(T, dt, dT, tau):
+def _transform_inlet_temp(T, dt, dT, tau):
+    """
+    Apply time offset, temperature offset, and windowing transform
+    to a temperature record.
+    
+    Parameters
+    ----------
+    T : 1-d numpy array or Pandas Series
+        Temperature series to be transformed.
+    dt : float
+        Constant representing the timelag of the loop system.
+    dT : float
+        Constant representing the change in water temperature
+        between the inlet and lab.
+    tau : integer
+        width of the averaging window in seconds.
+        
+    Returns
+    -------
+    T_prime : 1-d numpy array or Pandas Series
+        Transformed temperature series.
+        
+    """
+    
     T_prime = T.shift(int(dt / 5))
     T_prime = T_prime.rolling(int(tau / 5), center=False).mean()
     T_prime = T_prime + dT
+    
     return T_prime
 
 
-def errfunc(Tinlet, Tlab, dt, dT, tau):
-    Tinlet_prime = transform_inlet_temp(Tinlet, dt, dT, tau)
-    return ((Tlab - Tinlet) ** 2).sum()
+def _errfunc(Tinlet, Tlab, dt, dT, tau):
+    """
+    Error function used in minimization function.
+    
+    Parameters
+    ----------
+    Tinlet : 1-d array
+        Inlet temperature series.
+    Tlab : 1-d array
+        Lab temperature series.
+    dt : float
+        Time offset in seconds.
+    dT : float
+        Temperature offset in seconds.
+    tau : int
+        Window width in seconds.
+    
+    Returns
+    -------
+    sumsq : float
+        Sum of squares of residuals
+    
+    """
+    
+    Tinlet_prime = _transform_inlet_temp(Tinlet, dt, dT, tau)
+    sumsq = ((Tlab - Tinlet) ** 2).sum()
+    
+    return sumsq
+
+
+
+
+
