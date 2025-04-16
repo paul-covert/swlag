e0ff965f214b1bc90e4bf2c83714bc3bc3300078
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-04-16 07:53:43 -0700

    Moved _check_timeseries_spacing()

--- swlag/calc.py
+++ swlag/calc.py
@@ -3,30 +3,6 @@
 """Calculate transformed inlet temperatures and error functions"""
 
 
-def _check_timeseries_spacing(df):
-    """
-    Ensure uniformly, monotonically increasing time
-    
-    Parameters
-    ----------
-    df : Pandas dataframe indexed by datetime64
-        Contains the time (index), inlet temperature, and
-        lab temperature series.
-    
-    Returns
-    -------
-    monotonic : boolean
-        True of index of df is monotonically increasing.
-    
-    """
-    import numpy as np
-    
-    diff = np.diff(df.index.to_numpy())
-    monotonic = np.all(diff==diff[0])
-    
-    return monotonic
-
-
 def _transform_inlet_temp(T, dt, dT, tau):
     """
     Apply time offset, temperature offset, and windowing transform
--- swlag/utils.py
+++ swlag/utils.py
@@ -1,3 +1,26 @@
 # swlag: Lag and averaging time constant calculation in Python.
 # Copyright (C) 2025  Paul A. Covert (GNU GPLv3)
 """Convenience routines for calculation of time constants"""
+
+def _check_timeseries_spacing(df):
+    """
+    Ensure uniformly, monotonically increasing time
+    
+    Parameters
+    ----------
+    df : Pandas dataframe indexed by datetime64
+        Contains the time (index), inlet temperature, and
+        lab temperature series.
+    
+    Returns
+    -------
+    monotonic : boolean
+        True of index of df is monotonically increasing.
+    
+    """
+    import numpy as np
+    
+    diff = np.diff(df.index.to_numpy())
+    monotonic = np.all(diff==diff[0])
+    
+    return monotonic
