f44c5b049af0fff13874cab7adc2c0050bc63269
Paul Covert <82682319+paul-covert@users.noreply.github.com>
2025-04-17 12:06:06 -0700

    Update test_transform.py

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,7 +4,7 @@ import swlag as swl
 
 def test_transform():
     """Does _transform_inlet_tracer() return a new column?"""
-    path = "./data/example.csv"
+    path = "tests/data/example.csv"
     df = pd.read_csv(path)
     df["Tprime"] = swl.calc._transform_inlet_tracer(df["T"], 400., 0.3, 370.)
     assert isinstance(df["Tprime"], pd.Series)
