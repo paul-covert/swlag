eb6c56c13183ad4c0a6e1d5c186044f229fc58ff
Paul Covert <82682319+paul-covert@users.noreply.github.com>
2025-04-17 11:43:53 -0700

    Update test_transform.py

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -3,10 +3,10 @@ import swlag as swl
 
 
 def test_transform():
-    """Does transform_inlet_temp() return a new temperature column?"""
+    """Does _transform_inlet_tracer() return a new column?"""
     path = "./data/example.tob"
     df = swl.read_tob(path)
-    df["Tprime"] = swl.transform_inlet_temp(df["T"], 400., 0.3, 370.)
+    df["Tprime"] = swl._transform_inlet_tracer(df["T"], 400., 0.3, 370.)
     assert isinstance(df["Tprime"], pd.Series)
 
 
